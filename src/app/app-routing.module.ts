46b4541b622e6b3d5aa713461f3eb9317bb9e5cd
Raghunadh Barlapudi <81347796+9394113857@users.noreply.github.com>
2022-11-10 16:01:19 +0530

    First Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { UserListComponent } from './user-list/user-list.component';
+import { UserAddComponent } from './user-add/user-add.component';
+import { UserEditComponent } from './user-edit/user-edit.component';
+import { UserDetailComponent } from './user-detail/user-detail.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/user', pathMatch: 'full' },
+  { path: 'user', component: UserListComponent },
+  { path: 'detail/:id', component: UserDetailComponent },
+  { path: 'edit/:id', component: UserEditComponent },
+  { path: 'add', component: UserAddComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
